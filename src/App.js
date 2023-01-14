e91ce1da1bbbd6186032d677802ad06da9b0c855
Tomoka Takeuchi <tomoka@TomokanoMacBook-Pro.local>
2023-01-15 03:02:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = "React";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, {subject}！
         </p>
         <a
           className="App-link"
